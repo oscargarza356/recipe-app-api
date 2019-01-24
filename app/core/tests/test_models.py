4dabf1cf3418de20130f27e75efa7340afc38dd2
Oscar Garza <oscar35689@hotmail.com>
2019-01-24 15:23:42 -0600

    Added models, and test for users

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+# we are gonna test that our model can create a user
+from django.test import TestCase
+# get user model helper function as the user model we want to change it
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creatin a new user with an email is successful"""
+        email = "test@lgs.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@LONDONAPPDEV.COM"
+        user = get_user_model().objects.create_user(email,'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_super_user(
+            'test@lgs.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
