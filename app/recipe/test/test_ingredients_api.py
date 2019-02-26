db0d4408bcfaf72094ff12bef208aebca5b127cd
Garza <ogarza@lgsinnovations.com>
2019-02-26 12:29:12 -0600

    added create ingredient endpoints

--- app/recipe/test/test_ingredients_api.py
+++ app/recipe/test/test_ingredients_api.py
@@ -60,4 +60,15 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(len(res.data),1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
 
-    
+    def test_create_ingredient_succesfully(self):
+        """Test the api creation of an ingredient by the user"""
+        payload = {"name":"Chocolate"}
+        self.client.post(INGREDIENTS_URL,payload)
+        exists = Ingredient.objects.filter(user=self.user,name=payload['name']).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test invalid item creation"""
+        payload = {"name":""}
+        res = self.client.post(INGREDIENTS_URL,payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
